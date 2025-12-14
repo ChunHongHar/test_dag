d8157dd9736abf46e19baca16aa0b114570f422a
ChunHongHar <102952248+ChunHongHar@users.noreply.github.com>
2025-12-14 19:49:10 +0800

    fix: Induce eviction

--- listener_util.py
+++ listener_util.py
@@ -5,8 +5,8 @@ class Listener:
     def __init__(self, logger):
         self.listener_task: Task
         self.logger = logger
-        # if not self.conn:
-        #     raise Exception("DB connection could not be established")
 
     def _listen(self) -> None:
-        raise Exception
+        while True:
+            self.logger.warning("Listening for events...")
+        raise Exception("DB connection could not be established")
